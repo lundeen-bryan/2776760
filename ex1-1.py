bf5a83ae0d7953d46f03d21ca7426672c3f66c12
lundeen-bryan <lundeen-bryan@gmail.com>
2022-10-05 16:17:35 -0700

    finished exercises day1

---
+++ ex1-1.py
@@ -0,0 +1,5 @@
+""" print('Hello World!\nHello World!\nHello World!\n') """
+""" print("Hello " + "Angela") """
+""" name = input("What is your name?") """
+""" print(type(name)) """
+print('Hello ' + input("What is your name?"))
---
+++ ex1-3.py
@@ -0,0 +1,6 @@
+#ask for the users name
+name = input("What is your name?")
+#count the len of the name
+name_len = len(name)
+#print the number of characters in name
+print("Hello " + name + "!" + "\nYour name has " + str(name_len) + " characters.")
