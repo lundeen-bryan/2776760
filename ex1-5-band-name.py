655bd1b35b77a91b40f310a67bffb5c0b45bdc97
lundeen-bryan <lundeen-bryan@gmail.com>
2022-10-05 22:01:25 -0700

    updated code for use with py3

--- ex1-5-band-name.py
+++ ex1-5-band-name.py
@@ -1,13 +1,13 @@
-# Written for Python 2.7 not v3.x
+# Updated for Python 3.10.0 on 10/05/2022
 #1. Create a greeting for your program.
 new_line = '\n'
 print("Welcome to the Band Name Generator.")
 #2. Ask the user for the city that they grew up in.
 print(new_line)
-city = raw_input("What's the name of the city you grew up in? ")
+city = input("What's the name of the city you grew up in? ")
 #3. Ask the user for the name of a pet.
 print(new_line)
-pet = raw_input("What's the name of your pet? ")
+pet = input("What's the name of your pet? ")
 #4. Combine the name of their city and pet and show them their band name.
 print(new_line)
 print("Your band name is: " + city + " " + pet + ".")
