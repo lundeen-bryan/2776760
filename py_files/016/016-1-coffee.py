c262f1d97644f318cae4ef2477c5b3168bf187b5
lundeen-bryan <lundeen-bryan@gmail.com>
2022-10-22 09:56:56 -0700

    changed obj name convention & removed off fr menu

--- py_files/016/016-1-coffee.py
+++ py_files/016/016-1-coffee.py
@@ -4,20 +4,20 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 from _clear_console import clear
 
-BryBucksCoffeeMenu = Menu()
-CoffeeMachine = CoffeeMaker()
-CashRegister = MoneyMachine()
+brybucks_coffee_menu = Menu()
+coffee_machine = CoffeeMaker()
+cash_register = MoneyMachine()
 
 # Get the cost of a drink
 def return_cost(drink):
-  drink_menu_item = BryBucksCoffeeMenu.find_drink(drink)
+  drink_menu_item = brybucks_coffee_menu.find_drink(drink)
   drink_cost = drink_menu_item.cost
   return drink_cost
 
 # Check supply level
 def check_supply(drink):
-  drink_menu_item = BryBucksCoffeeMenu.find_drink(drink)
-  supply_sufficient = CoffeeMachine.is_resource_sufficient(drink_menu_item)
+  drink_menu_item = brybucks_coffee_menu.find_drink(drink)
+  supply_sufficient = coffee_machine.is_resource_sufficient(drink_menu_item)
   if supply_sufficient == False:
     print(f"We're sorry we don't have your {drink} available at this time.")
   return supply_sufficient
@@ -25,7 +25,7 @@ def check_supply(drink):
 # Show user menu
 def show_menu():
   menu = []
-  menu = BryBucksCoffeeMenu.get_items()
+  menu = brybucks_coffee_menu.get_items()
   drink_names_list = menu.split("/")
   x = 0
   for ea in range(len(drink_names_list) - 1):
@@ -33,12 +33,13 @@ def show_menu():
     drink_cost = "${:,.2f}".format(return_cost(drink_names_list[ea]))
     print(f"{x}) {drink_names_list[ea]} - {drink_cost}")
   print("\nPlease pick a drink from the list above by entering a number between 1-3.")
-  print("Type (O) to turn off the machine.")
+#  print("Type (O) to turn off the machine.")
+# Off is a secret command
   print("Type (R) for a report of supplies.")
 
 # Process coins
 def count_coins_entered(total_price):
-  coins = CashRegister.COIN_VALUES
+  coins = cash_register.COIN_VALUES
   money = 0.0
   due = total_price
   total_paid = 0
@@ -59,7 +60,7 @@ def count_coins_entered(total_price):
   if due < 0:
     change = due * (-1)
     total_paid = total_price + change
-  CashRegister.make_payment(total_price, total_paid)
+  cash_register.make_payment(total_price, total_paid)
 
 def end():
   print("Have a nice day. Thank you. Come again. 😊")
@@ -78,14 +79,14 @@ def main():
     print("Welcome to BryBucks Coffee! Please select from our available menu:")
     show_menu()
     selected_drink = input("> ").upper().strip()
-    if selected_drink == "O":
+    if selected_drink == "O" or selected_drink == "OFF":
       print("Shutting down...")
       time.sleep(3)
       print("Goodbye")
       exit(0)
     elif selected_drink == "R":
-      CoffeeMachine.report()
-      CashRegister.report()
+      coffee_machine.report()
+      cash_register.report()
       input("Press ENTER to continue...")
     elif selected_drink == "1":
       drink = "latte"
@@ -101,7 +102,7 @@ def main():
     display_drink_price = "${:,.2f}".format(drink_price)
     print(f"Please insert {display_drink_price}")
     count_coins_entered(drink_price)
-    print(CoffeeMachine.make_coffee(BryBucksCoffeeMenu.find_drink(drink)))
+    print(coffee_machine.make_coffee(brybucks_coffee_menu.find_drink(drink)))
     print("Enjoy!")
   return # return to main
 
