cb09a416a3379ffff855b84ff0dbfa3e50fcb4e3
lundeen-bryan <lundeen-bryan@gmail.com>
2022-10-26 16:16:44 -0700

    finished Day20

--- src/020/020-0.py
+++ src/020/020-0.py
@@ -1,44 +1,28 @@
-import turtle as tur
+from turtle import Screen
+from snake import Snake
 import time
 
-scr = tur.Screen()
+DISTANCE = 20
+
+scr = Screen()
 scr.setup(width=688, height=688)
 scr.bgcolor("black")
 scr.title("My Snake Game")
 
-segments = []
-snake_xy = [0,0]
+snake = Snake()
 
-x=0
-y=0
-a = 20 # distance between turtles (also size)
-dist = 20
-
-scr.tracer(0, 25)
-
-for ea in range(3):
-  name = f"seg_{ea}"
-  name = tur.Turtle(shape="square")
-  name.penup()
-  name.color("white")
-  if ea > 0:
-    snake_xy[0] += a
-  name.goto(snake_xy[0], snake_xy[1])
-  segments.append(name)
+scr.listen()
+scr.onkey(snake.up, "Up")
+scr.onkey(snake.down, "Down")
+scr.onkey(snake.left, "Left")
+scr.onkey(snake.right, "Right")
 
 
 game_on = True
-count = 0 # temporarily use counter to stop loop
 while game_on:
-  scr.update()
-  for seg in segments:
-    if count > 30:
-      game_on = False
-      break
-    for seg_num in range(len(segments) - 1, 0, -1):
-      segments[seg_num].goto()
-    seg.forward(dist)
-    count += 1
-    time.sleep(0.05)
+  scr.update() # update screen after segments move
+  time.sleep(0.1) # control the speed
+  snake.move_snake()
+
 
 scr.exitonclick()
